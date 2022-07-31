012631dea1a6e9d146b72e2f3496a8924e8fc4e3
Harley <harley201009@hotmail.com>
2022-07-31 17:14:45 -0300

    Implementing delete and save integration tests.

--- src/test/java/com/harley/library/repositories/BookRepositoryTest.java
+++ src/test/java/com/harley/library/repositories/BookRepositoryTest.java
@@ -7,11 +7,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -50,8 +53,50 @@ class BookRepositoryTest {
 
         // Verification
         Assertions.assertThat(exist).isFalse();
-
     }
+    @Test
+    @DisplayName("Must find a book by id")
+    public void findBookById() {
+        // Scenary
+        Book book = createValidBook();
+        testEntityManager.persist(book);
+
+        // Execution
+        Optional<Book> foundBook = bookRepository.findById(book.getId());
+
+        // Verification
+        Assertions.assertThat(foundBook.isPresent()).isTrue();
+    }
+
+    @Test
+    @DisplayName("Must save a book")
+    public void saveBook() {
+        // Scenary
+        Book book = createValidBook();
+
+        // Executation
+        Book saveBook = bookRepository.save(book);
+
+        // Verification
+        Assertions.assertThat(saveBook.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Must delete a book")
+    public void deleteBook() {
+        // Scenary
+        Book book = createValidBook();
+        testEntityManager.persist(book);
+
+        // Execution
+        Book foundBook = testEntityManager.find(Book.class, book.getId());
+        bookRepository.delete(foundBook);
+
+        // Verification
+        Book deleteBook = testEntityManager.find(Book.class, book.getId());
+        Assertions.assertThat(deleteBook).isNull();
+    }
+
 
     private Book createValidBook() {
         return Book.builder().title("My Adventures").author("Mary").isbn("1234").build();
